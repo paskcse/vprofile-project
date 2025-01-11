43142e5077a71b287a87afaa4a812359210ccfb9
Sathish Kumar P A <paskcse@gmail.com>
2025-01-11 08:56:32 +0530

    Update tomcat.sh

--- vagrant/Automated_provisioning_WinMacIntel/tomcat.sh
+++ vagrant/Automated_provisioning_WinMacIntel/tomcat.sh
@@ -1,61 +1,41 @@
-TOMURL="https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.75/bin/apache-tomcat-9.0.75.tar.gz"
-dnf -y install java-11-openjdk java-11-openjdk-devel
-dnf install git maven wget -y
+#!/bin/bash
+DATABASE_PASS='admin123'
+sudo yum update -y
+sudo yum install epel-release -y
+sudo yum install git zip unzip -y
+sudo yum install mariadb-server -y
+
+
+# starting & enabling mariadb-server
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
 cd /tmp/
-wget $TOMURL -O tomcatbin.tar.gz
-EXTOUT=`tar xzvf tomcatbin.tar.gz`
-TOMDIR=`echo $EXTOUT | cut -d '/' -f1`
-useradd --shell /sbin/nologin tomcat
-rsync -avzh /tmp/$TOMDIR/ /usr/local/tomcat/
-chown -R tomcat.tomcat /usr/local/tomcat
-
-rm -rf /etc/systemd/system/tomcat.service
-
-cat <<EOT>> /etc/systemd/system/tomcat.service
-[Unit]
-Description=Tomcat
-After=network.target
-
-[Service]
-
-User=tomcat
-Group=tomcat
-
-WorkingDirectory=/usr/local/tomcat
-
-#Environment=JRE_HOME=/usr/lib/jvm/jre
-Environment=JAVA_HOME=/usr/lib/jvm/jre
-
-Environment=CATALINA_PID=/var/tomcat/%i/run/tomcat.pid
-Environment=CATALINA_HOME=/usr/local/tomcat
-Environment=CATALINE_BASE=/usr/local/tomcat
-
-ExecStart=/usr/local/tomcat/bin/catalina.sh run
-ExecStop=/usr/local/tomcat/bin/shutdown.sh
-
-
-RestartSec=10
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-
-EOT
-
-systemctl daemon-reload
-systemctl start tomcat
-systemctl enable tomcat
-
 git clone -b main https://github.com/paskcse/vprofile-project.git
-cd vprofile-project
-mvn install
-systemctl stop tomcat
-sleep 20
-rm -rf /usr/local/tomcat/webapps/ROOT*
-cp target/vprofile-v2.war /usr/local/tomcat/webapps/ROOT.war
-systemctl start tomcat
-sleep 20
-systemctl stop firewalld
-systemctl disable firewalld
-#cp /vagrant/application.properties /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/application.properties
-systemctl restart tomcat
+# Setting the root password and securing the installation
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DATABASE_PASS';"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='';"
+sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';"
+sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES;"
+
+# Creating the database and granting privileges
+sudo mysql -u root -p"$DATABASE_PASS" -e "CREATE DATABASE accounts;"
+sudo mysql -u root -p"$DATABASE_PASS" -e "GRANT ALL PRIVILEGES ON accounts.* TO 'admin'@'localhost' IDENTIFIED BY '$DATABASE_PASS';"
+sudo mysql -u root -p"$DATABASE_PASS" -e "GRANT ALL PRIVILEGES ON accounts.* TO 'admin'@'%' IDENTIFIED BY '$DATABASE_PASS';"
+sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES;"
+
+# Restoring the database dump
+sudo mysql -u root -p"$DATABASE_PASS" accounts < /tmp/vprofile-project/src/main/resources/db_backup.sql
+
+
+# Restart mariadb-server
+sudo systemctl restart mariadb
+
+
+#starting the firewall and allowing the mariadb to access from port no. 3306
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+sudo firewall-cmd --get-active-zones
+sudo firewall-cmd --zone=public --add-port=3306/tcp --permanent
+sudo firewall-cmd --reload
+sudo systemctl restart mariadb
